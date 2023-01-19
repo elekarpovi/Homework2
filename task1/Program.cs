2a7c0df834839b72809177e51b1e9115f7855cab
Elena Karpovich <elekarpovi@tut.by>
2023-01-19 22:45:44 +0300

    task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+System.Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ((a<1000) && (a>99) || (a<-99) && (a>-1000))
+{
+    int firstTwoNumbers = a / 10;
+    int secondNumber = firstTwoNumbers % 10;
+    System.Console.WriteLine($"Вторая цифра числа: {Math.Abs(secondNumber)}");
+}
+
+else
+{
+    System.Console.WriteLine($"Ошибка, введите трехзначное число!");
+}
